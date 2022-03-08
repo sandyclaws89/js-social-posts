ca260bcbc32048a1fd2bd41b6ade3f2d6a00c31b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-08 16:13:57 +0100

    Completate schede con modifiche

--- script/main.js
+++ script/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 const posts = [
     {
         "id": 1,
@@ -8,7 +12,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "created": "2021-06-25"
+        "created": "02-06-21"
     },
     {
         "id": 2,
@@ -19,7 +23,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=10"
         },
         "likes": 120,
-        "created": "2021-09-03"
+        "created": "11-09-21"
     },
     {
         "id": 3,
@@ -30,7 +34,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 78,
-        "created": "2021-05-15"
+        "created": "06-05-21"
     },
     {
         "id": 4,
@@ -41,7 +45,7 @@ const posts = [
             "image": null
         },
         "likes": 56,
-        "created": "2021-04-03"
+        "created": "07-04-21"
     },
     {
         "id": 5,
@@ -52,48 +56,50 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-        "created": "2021-03-05"
+        "created": "03-03-21"
     }
 ];
 
 const containerJs = document.getElementById('container');
 containerJs.classList.add ('posts-list');
-console.log ('ciao');
+// const 
+// console.log ('ciao');
+
 
 for(let i=0; i< posts.length; i++){ 
 
-const postJs = document.createElement ('div');
-
-postJs.innerHTML= `<div class="post">
-<div class="post__header">
-    <div class="post-meta">                    
-        <div class="post-meta__icon">
-            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-        </div>
-        <div class="post-meta__data">
-            <div class="post-meta__author">Phil Mangione</div>
-            <div class="post-meta__time">4 mesi fa</div>
-        </div>                    
-    </div>
-</div>
-<div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-<div class="post__image">
-    <img src="https://unsplash.it/600/300?image=171" alt="">
-</div>
-<div class="post__footer">
-    <div class="likes js-likes">
-        <div class="likes__cta">
-            <a class="like-button  js-like-button" href="#" data-postid="1">
-                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                <span class="like-button__label">Mi Piace</span>
-            </a>
-        </div>
-        <div class="likes__counter">
-            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-        </div>
-    </div> 
-</div>            
-</div>`;
-
-containerJs.append(postJs);
+    console.log(posts[i].author)
+            const postJs = document.createElement ('div');
+            postJs.innerHTML= `<div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="Name">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+            </div>`;
+            containerJs.append(postJs);
+      
 }
