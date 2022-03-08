2cbc36c97ff26a96b49edc004e61ffdf77cb7033
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-08 16:49:50 +0100

    Tentativi funzionamento btn like

--- script/main.js
+++ script/main.js
@@ -1,4 +1,26 @@
-
+/*Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Nel file js avete un array di oggetti che rappresentano ciascun post.
+Ogni post contiene le informazioni necessarie per stampare la relativa card:
+id del post (numero progressivo da 1 a n),
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+BONUS
+1. Formattare le date in formato italiano (gg/mm/aaaa)
+2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+Consigli del giorno:
+Ragioniamo come sempre a step.
+Prima scriviamo nei commenti la logica in italiano e poi traduciamo in codice.
+console.log() è nostro amico (provate anche la variante console.table() per array e oggetti, o ancora meglio con array di oggetti).
+Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
+Buon lavoro!*/
 
 
 
@@ -67,7 +89,7 @@ containerJs.classList.add ('posts-list');
 
 
 for(let i=0; i< posts.length; i++){ 
-
+    
     console.log(posts[i].author)
             const postJs = document.createElement ('div');
             postJs.innerHTML= `<div class="post">
@@ -100,6 +122,25 @@ for(let i=0; i< posts.length; i++){
                 </div> 
             </div>            
             </div>`;
-            containerJs.append(postJs);
-      
+        containerJs.append(postJs);
+        let likeBtn = document.querySelector('.like-button');
+        likeBtn.addEventListener('click', like);
+
+        function like(event){
+            likeBtn.classList.add('like-button--liked');
+           
+
+            let likedJs = document.getElementById('like-counter-1');
+            likedJs.value = likedJs++
+            console.log(likedJs);
+            event.preventDefault()
+
+                
+            }
 }
+
+
+
+
+
+
